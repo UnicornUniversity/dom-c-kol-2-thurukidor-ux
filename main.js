125d38ed237594b4090d96bf688ac0c00d7cd77f
thurukidor-ux <thuru.kidor@gmail.com>
2025-11-13 23:48:35 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,34 @@
-// Říká testům, že vstup bude binární (základ 2)
-function permittedInputSystems() {
+// vstup bude binární (základ 2)
+export function permittedInputSystems() {
     return 2; 
 }
 
-// Říká testům, že výstup bude dekadický (základ 10)
-function permittedOutputSystems() {
+// výstup bude dekadický (základ 10)
+export function permittedOutputSystems() {
     return 10; 
 }
 
-/**
- * převod čísel 
- * @param {string} inputNumber binární číslo 
- * @returns {string} číslo v desítkové soustavě 
- */
-function main(inputNumber, inputNumberSystem, outputNumberSystem) {
+
+export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
     
-    // (2 -> 10).
+    // (2 -> 10)
     if (inputNumberSystem !== 2 || outputNumberSystem !== 10) {
         return "Chyba: Nepodporovaná konverze"; 
     }
 
     let dekadickyVysledek = 0;
-    let mocnina = 1; //
+    let mocnina = 1; // 2^0 = 1
 
     // procházíme binární číslo 
     for (let i = inputNumber.length - 1; i >= 0; i--) {
         const cislice = inputNumber[i];
         
-        // Pokud je bit '1', přičteme aktuální mocninu dvojky k výsledku
+        // pokud je bit '1', přičteme aktuální mocninu dvojky k výsledku
         if (cislice === '1') {
             dekadickyVysledek = dekadickyVysledek + mocnina;
         } 
         
-        // zdvojnásobíme mocninu pro další pozici vlevo
+        // zdvojnásobíme mocninu pro další pozici 
         mocnina = mocnina * 2; 
     }
 
